ccd18cdc690b30985e298132ff845ff17f51d4a9
Christopher Martin <ch.martin@gmail.com>
2012-07-16 16:49:51 -0400

    got test.m invoking spca, seems to work

--- spca/test.m
+++ spca/test.m
@@ -1,13 +1,27 @@
 #! /usr/bin/octave -qf
 
 addpath('spasm')
+addpath('../matlab')
 
-function w = test()
-  range = [0 0 10 10]
+function Y = normalize(X)
+  m = mean(X);
+  v = diag(cov(X))' .^ (1/2);
+  Y = (X .- repmat(m, size(X, 1), 1)) ./ repmat(v, size(X, 1), 1);
+endfunction
+
+function test()
+  range = [0 0 50 50]
   K = 4
   stop = 0
   X = dlmread('../face/data', ' ', range);
+  X = normalize(X);
+  sum(var(X))
   w = spca(X, [], K, inf, stop);
+  p = X * w;
+  sum(var(p))
+  w = normc(rand(size(X, 1), K));
+  p = X * w;
+  sum(var(p))
 endfunction
 
 test()
