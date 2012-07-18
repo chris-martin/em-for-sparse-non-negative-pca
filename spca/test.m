7a7f038787f63673fe0887ba53cd5070c75336fd
Christopher Martin <ch.martin@gmail.com>
2012-07-18 00:59:58 -0400

    making more sense of spca

--- spca/test.m
+++ spca/test.m
@@ -6,23 +6,28 @@ addpath('../matlab')
 function Y = normalize(X)
   m = mean(X);
   v = diag(cov(X))' .^ (1/2);
-  Y = (X .- repmat(m, size(X, 1), 1)) ./ repmat(v, size(X, 1), 1);
+  Y = (X .- repmat(m, size(X, 1), 1)) \
+      ./ repmat(v, size(X, 1), 1);
+endfunction
+
+function p = project(A, v)
+  p = A * inv(A' * A) * A' * v;
+endfunction
+
+function v = variance(w, X)
+  v = sum(var(project(w, X')'));
 endfunction
 
 function test()
-  range = [0 0 50 50]
-  K = 4
-  stop = 0
+  N = 150 % number of samples
+  D = 361 % dimension of sample data
+  K = 1 % number of principle components
+  c = 150 % cardinality (nonzero variables)
+  range = [0 0 (N - 1) (D - 1)];
   X = dlmread('../face/data', ' ', range);
   X = normalize(X);
-  sum(var(X))
-  w = spca(X, [], K, inf, stop);
-  p = X * w;
-  sum(var(p))
-  w = normc(rand(size(X, 1), K));
-  p = X * w;
-  sum(var(p))
+  variance(spca(X, [], K, inf, -c), X)
+  variance(normc(rand(D, K)), X)
 endfunction
 
 test()
-
