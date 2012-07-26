606cf8b2f3ff7ef91d2eef38427f770b2f91bc3d
Christopher Martin <ch.martin@gmail.com>
2012-07-26 12:26:07 -0400

    renormalization in spca

--- spca/test.m
+++ spca/test.m
@@ -1,7 +1,7 @@
 #! /usr/bin/octave -qf
 
-addpath('spasm')
 addpath('../matlab')
+addpath('/usr/share/octave/packages/3.2/statistics-1.0.10')
 
 % Normalizes the columns of the data matrix
 % to have mean and variance 1.
@@ -27,14 +27,26 @@ endfunction
 % c : cardinality (nonzero variables)
 % X : normalized data matrix
 % v : variance of data after dimension reduction
-function a, v, t = test(c, X)
+function a, v, t = test(c, X, renormalize)
   c
   if c == 0;
     t = 0;
     v = 0;
   else
     t = cputime;
+    addpath('spasm')
     w = spca(X, [], 1, inf, -c);
+    rmpath('spasm')
+    if renormalize;
+      w2 = [];
+      for i = 1:size(w, 1);
+        if w(i) ~= 0;
+          w2 = [w2; i];
+        end
+      end
+      X = X(:,w2');
+      w = princomp(X)(:, 1);
+    end
     t = cputime - t
     v = variance(w, X)
   end
@@ -58,16 +70,16 @@ endfunction
 
 data = face();
 data = normalize(data);
-step = 50;
+step = 1;
 x1 = 0:step:150;
-x2 = 0:step:400;
-q = arrayfun(@(c) test(c, data), x2, 'UniformOutput', false);
+x2 = 0:step:150;
+q = arrayfun(@(c) test(c, data, 1), x2, 'UniformOutput', false);
 r = reshape([q{:}], 2, size(x2, 2));
 y = r(1,1:size(x1, 2))
 t = r(2,:)
 csvwrite('/tmp/a.csv', [x1' y'])
 scatter(x1, y, 10, [0 0 1], 's')
 print('/tmp/a.png', '-dpng', '-S500,500')
-clf()
-scatter(x2, t, 10, [1 0 0], 's')
-print('/tmp/b.png', '-dpng', '-S500,500')
+%clf()
+%scatter(x2, t, 10, [1 0 0], 's')
+%print('/tmp/b.png', '-dpng', '-S500,500')
